3aea82b58359bb1afdddb9f83288d4a7271ca17a
Jason Lazzuri and Peter Karellas <jason@>
2017-03-27 11:05:57 -0700

    add loops to shorten the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
 
-    var birdInput = $("input#bird").val();
+    var input = ["person1","person2", "animal","exclamation","verb","noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    input.forEach(function(inputs){
+      var userInput = $("#" + input).val();
+      $("." + input).text(userInput);
+    });
 
-    $(".bird").text(birdInput);
+    // var person1Input = $("#person1").val();
+    // var person2Input = $("#person2").val();
+    // var animalInput = $("#animal").val();
+    // var exclamationInput = $("#exclamation").val();
+    // var verbInput = $("#verb").val();
+    // var nounInput = $("#noun").val();
+    //
+    // var birdInput = $("input#bird").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+    //
+
 
     $("#story").show();
 
